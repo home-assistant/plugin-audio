b8328504b30ff98a512388780480a2e101d11e46
Pascal Vizeli <pvizeli@syshack.ch>
2020-02-24 20:36:12 +0100

    fix logoutput

--- rootfs/etc/cont-init.d/adjust-audio-group.sh
+++ rootfs/etc/cont-init.d/adjust-audio-group.sh
@@ -6,17 +6,17 @@ HOST_AUDIO_GID=$(grep "^audio" /etc/group_host | cut -d':' -f 3)
 LOCAL_AUDIO_GID=$(grep "^audio" /etc/group | cut -d':' -f 3)
 
 # Need processing?
-bashio::log.info "Host GUID: ${HOST_AUDIO_GID} - Local GUID: ${LOCAL_AUDIO_GID}"
+bashio::log.info "Host GID: ${HOST_AUDIO_GID} - Local GID: ${LOCAL_AUDIO_GID}"
 if [ "${HOST_AUDIO_GID}" == "${LOCAL_AUDIO_GID}" ]; then
     bashio::exit.ok
 fi
 
 # Make sure the new GID is unique
 if grep ":${HOST_AUDIO_GID}:"; then
-    bashio::log.warning "Fix group id permission"
+    bashio::log.warning "Fix GID permission"
     GROUP_NAME=$(grep ":${HOST_AUDIO_GID}:" /etc/group | cut -d':' -f 0)
     addgroup pulse ${GROUP_NAME}
 else
-    bashio::log.info "Update local gid for audio"
+    bashio::log.info "Update local GID for audio"
     sed -i "s/audio:x:${LOCAL_AUDIO_GID}:/audio:x:${HOST_AUDIO_GID}:/g" /etc/group
 fi
