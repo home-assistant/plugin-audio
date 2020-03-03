1e237ab2f0c9dc0c1e5ef2ca076e32d2b753a670
Pascal Vizeli <pvizeli@syshack.ch>
2020-03-03 14:23:04 +0100

    Host have no audio support (#3)
    * Host have no audio support
    
    * Update adjust-audio-group.sh
    
    * Update rootfs/etc/cont-init.d/adjust-audio-group.sh
    
    Co-Authored-By: Franck Nijhof <git@frenck.dev>
    
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- rootfs/etc/cont-init.d/adjust-audio-group.sh
+++ rootfs/etc/cont-init.d/adjust-audio-group.sh
@@ -2,8 +2,14 @@
 # ==============================================================================
 # Adjust host audio group with gid inside container
 # ==============================================================================
-HOST_AUDIO_GID=$(grep "^audio" /host/group | cut -d':' -f 3)
-LOCAL_AUDIO_GID=$(grep "^audio" /etc/group | cut -d':' -f 3)
+if [ ! -d /dev/snd ]; then
+    bashio:log.warning "The host has no audio support!"
+    bashio::exit.ok
+fi
+
+# Get GID data
+HOST_AUDIO_GID="$(grep "^audio" /host/group | cut -d':' -f 3)"
+LOCAL_AUDIO_GID="$(grep "^audio" /etc/group | cut -d':' -f 3)"
 
 # Need processing?
 bashio::log.info "Host GID: ${HOST_AUDIO_GID} - Local GID: ${LOCAL_AUDIO_GID}"
@@ -15,7 +21,7 @@ fi
 if grep ":${HOST_AUDIO_GID}:"; then
     bashio::log.warning "Fix GID permission"
     GROUP_NAME=$(grep ":${HOST_AUDIO_GID}:" /etc/group | cut -d':' -f 0)
-    addgroup pulse ${GROUP_NAME}
+    addgroup pulse "${GROUP_NAME}"
 else
     bashio::log.info "Update local GID for audio"
     sed -i "s/audio:x:${LOCAL_AUDIO_GID}:/audio:x:${HOST_AUDIO_GID}:/g" /etc/group
