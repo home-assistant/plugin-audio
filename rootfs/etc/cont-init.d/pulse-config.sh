22f426313e0574e821cd964158b6eac3e741b2dc
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-02 17:57:07 +0200

    Fix pulse config (#19)

---
+++ rootfs/etc/cont-init.d/pulse-config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Check device information and adjust Pulse
+# ==============================================================================
+ALSA_CARDS="$(aplay -L)"
+
+# BCM2835
+if echo "${ALSA_CARDS}" | grep "[bcm2835 ALSA]"; then
+    bashio::log.info "Found BCM2835 card"
+
+    sed -i "s/module-udev-detect/module-udev-detect tsched=0/" /etc/pulse/system.pa
+fi
