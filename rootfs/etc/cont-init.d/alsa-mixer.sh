35bfe603f19e3192cea074aa104b776e819d2b06
Pascal Vizeli <pvizeli@syshack.ch>
2020-06-29 15:57:48 +0200

    Fix alsa mixer settings on startup (#27)

---
+++ rootfs/etc/cont-init.d/alsa-mixer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Initial ALSA mixer adjustments 
+# ==============================================================================
+
+# Guard if no sound support found
+if [ ! -d /dev/snd ]; then
+    bashio::log.warning "Can't find sound device for ALSA mixer settings"
+    bashio::exit.ok
+fi
+
+# Loop over all Controls
+for control in /dev/snd/control*
+do
+    bashio::log.info "Adjust ALSA mixer settings for $control"
+    soundconfig $control
+done
