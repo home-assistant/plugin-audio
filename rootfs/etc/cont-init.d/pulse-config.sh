19e2bde2a1e658fce4d3374ec089266cd8420ab5
Pascal Vizeli <pvizeli@syshack.ch>
2020-08-05 17:02:51 +0200

    Change scheduler N2

--- rootfs/etc/cont-init.d/pulse-config.sh
+++ rootfs/etc/cont-init.d/pulse-config.sh
@@ -4,9 +4,14 @@
 # ==============================================================================
 ALSA_CARDS="$(aplay -l)"
 
-# BCM2835
+# RaspberryPi
 if echo "${ALSA_CARDS}" | grep -q "\[bcm2835 ALSA\]"; then
-    bashio::log.info "Found BCM2835 card"
+    bashio::log.info "Found RaspberryPi system"
+
+    sed -i "s/module-udev-detect/module-udev-detect tsched=0/" /etc/pulse/system.pa
+# Odroid N2
+elif echo "${ALSA_CARDS}" | grep -q "\[G12B-ODROID-N2\]"; then
+    bashio::log.info "Found Odroid N2 system"
 
     sed -i "s/module-udev-detect/module-udev-detect tsched=0/" /etc/pulse/system.pa
 fi
