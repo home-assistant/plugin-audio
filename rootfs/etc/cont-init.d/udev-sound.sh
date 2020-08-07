e47fe94f5c45dfa60c8c5de89483a97651ce850c
Pascal Vizeli <pvizeli@syshack.ch>
2020-08-07 09:08:13 +0200

    Make udev more robust

--- rootfs/etc/cont-init.d/udev-sound.sh
+++ rootfs/etc/cont-init.d/udev-sound.sh
@@ -4,6 +4,9 @@
 # ==============================================================================
 udevd --daemon
 
-bashio::log.info "Update udev subsystem sounds"
-udevadm trigger --subsystem-match=sound
-udevadm settle
+bashio::log.info "Update udev subsystem sound"
+if udevadm trigger --subsystem-match=sound; then
+    udevadm settle || true
+else
+    bashio::log.warning "Triggering of sound udev rules fails!"
+fi
