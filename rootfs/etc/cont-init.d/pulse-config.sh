bf390bfd2e5695e2ff5e6d03b6640f124e66f32c
Pascal Vizeli <pvizeli@syshack.ch>
2021-02-13 15:03:39 +0100

    Use tempio to generate config (#48)

--- rootfs/etc/cont-init.d/pulse-config.sh
+++ rootfs/etc/cont-init.d/pulse-config.sh
@@ -2,16 +2,26 @@
 # ==============================================================================
 # Check device information and adjust Pulse
 # ==============================================================================
-ALSA_CARDS="$(aplay -l)"
+declare tsched
+readonly ALSA_CARDS="$(aplay -l)"
 
 # RaspberryPi
 if echo "${ALSA_CARDS}" | grep -q "\[bcm2835 ALSA\]"; then
     bashio::log.info "Found RaspberryPi system"
+    tsched=false
 
-    sed -i "s/module-udev-detect/module-udev-detect tsched=0/" /etc/pulse/system.pa
 # Odroid N2
 elif echo "${ALSA_CARDS}" | grep -q "\[G12B-ODROID-N2\]"; then
     bashio::log.info "Found Odroid N2 system"
+    tsched=false
 
-    sed -i "s/module-udev-detect/module-udev-detect tsched=0/" /etc/pulse/system.pa
+else
+    tsched=true
 fi
+
+# Generate config
+bashio::var.json \
+    tsched "^${tsched}" \
+    | tempio \
+        -template /usr/share/tempio/system.pa \
+        -out /etc/pulse/system.pa
