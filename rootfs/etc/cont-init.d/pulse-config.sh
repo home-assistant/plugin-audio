7fef969e8e73f2f1cfb4d6148ed5c6c011b7ec56
Pascal Vizeli <pvizeli@syshack.ch>
2021-02-15 15:25:55 +0100

    Update pulse-config.sh

--- rootfs/etc/cont-init.d/pulse-config.sh
+++ rootfs/etc/cont-init.d/pulse-config.sh
@@ -6,7 +6,7 @@ declare tsched
 readonly ALSA_CARDS="$(aplay -l)"
 
 # RaspberryPi
-if [[ "${ALSA_CARDS}" =~ "\[bcm2835 .*\]" ]]; then
+if [[ "${ALSA_CARDS}" =~ \[bcm2835.*?\] ]]; then
     bashio::log.info "Found RaspberryPi system"
     tsched=false
 else
